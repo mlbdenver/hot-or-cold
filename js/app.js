066b174402e4901c38265b2c4e30c6ff7f3680f5
MonicaB <mlbdenver@gmail.com>
2014-05-12 14:43:15 -0600

    Still a mess
    Still messed up pretty bad. I can’t figure out where its going wrong

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 
-$(document).ready(function(){
+$(document).ready(function() {
 	//focus on input
     $("#userGuess").focus();
     //Clear all from old game
     //Declare variables
     var numGuesses = 0;
-
+    var diff = 0;
+    var guess;
+   
     //Generate random number and assign to variable
-    var target = Math.floor(Math.random() * 101);
+    var target = Math.floor((Math.random() * 100) + 1);
     console.log("Your random number is " + target);
 
     //Accept guess
     $("#guessButton").click(validateGuess);
+
     /*make enter submit button - this was happening automatically, so I disabled
     $("#userGuess").keyup(function(event) {
         if(event.keyCode == 13){
@@ -20,55 +23,59 @@ $(document).ready(function(){
     });*/
 
     function validateGuess() {
-        var guess = $('#userGuess').val();
+        guess = $('#userGuess').val();
         console.log("Guess is " + guess);
+        guess = +guess;
         // check to make sure guess is not empty
-        if(!guess ||!guess.trim()) {
+        /*if(!guess ||!guess.trim()) {
             alert("You forgot to enter your item");
         }
         //check to make sure guess is not NaN
-        else if(isNan(+guess)) {
-            alert("You must enter a number between 1 and 100");
+        else if(isNaN(guess)) {
+            alert("You must enter a number");
         }
         //check to make sure guess is between 1 and 100
-        else if(+guess <= 0 && +guess > 100) {
+        else if(guess <= 0 && guess > 100) {
             alert("You must enter a number between 1 and 100");
         }
         //If all well, compare guess to random
-        else compareGuess(+guess); //not sure this goes here
-        console.log("Your guess is " + userGuess);
-        addguess(guess);
+        else { */
+            compareGuess(+guess); //not sure this goes here
+            console.log("Your guess is " + guess);
+            //}
     }
     
-    
-    var diff = guess - target;
-    if (diff > 50) {
-        console.log("Diff greater than 50");
-    }
-    else if (diff < 40) {
-        console.log("Diff less than 40");
-    }
-    else if (diff < 25) {
-        console.log("Diff less than 25");
-    }
-    else if (diff < 10) {
-        console.log("Diff less than 10");
-    }
-   
-    
-    function addGuess() {
-        numGuesses++;
-        $('#guessList').apend("<li>" + guess + "</li>");
-    }
+    function compareGuess(guess) {
+        diff = guess - target;
+            if (diff === 0) {
+                console.log("Yay!!!!!");
+            }
+            else if (diff < 10) {
+                console.log("Diff less than 10");
+            }
+            else if (diff < 25) {
+                console.log("Diff less than 25");
+            }
+            else if (diff < 40) {
+                console.log("Diff less than 40");
+            }
+            else {
+                console.log("Diff greater than 40");
+            }
+            numGuesses++;
+            console.log("The number of total guesses is " + numGuesses);
+            $('#guessList').append("<li>" + guess + "</li>");
+            
+        }
+   });
 
-    addGuess(guess);
-    
+  
     
         
 
     
         //$("#newitem").val(''); //clear new item entry box
-    });
+   
         //Check to make sure guess is between 1 and 100
         //Display message if not
     //Assign variable to guess
