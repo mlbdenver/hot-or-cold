291ac0972f62f19ae0f5ab8d353401fda26c9f49
MonicaB <mlbdenver@gmail.com>
2014-05-17 00:56:28 -0600

    Think I got it
    Seems to work now. Code not pretty, I think there is some random
    functionality in there.

--- js/app.js
+++ js/app.js
@@ -1,106 +1,118 @@
 
 $(document).ready(function() {
-	//focus on input
+	/*--- focus on input ---*/
     $("#userGuess").focus();
-    //Clear all from old game
-    //Declare variables
+
+   /*--- Display information modal box ---*/
+    $(".what").click(function(){
+        $(".overlay").fadeIn(1000);
+    });
+
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function(){
+        $(".overlay").fadeOut(1000);
+    });
+
+    /*--- Start New Game ---*/
+    $(".new").click(function() {
+        newGame();
+    });
+ 
+    /*---Declare variables---*/
     var numGuesses = 0;
-    var diff = 0;
+    var diff;
     var guess;
+    var finish = false;
    
-    //Generate random number and assign to variable
+    /*---Generate random number and assign to variable---*/
     var target = Math.floor((Math.random() * 100) + 1);
     console.log("The random number is " + target);
 
-    function validateGuess() {
-        guess = $('#userGuess').val();
-        console.log("Guess is " + guess);
-        guess = +guess;
-        // check to make sure guess is not empty
-        /*if(!guess ||!guess.trim()) {
-            alert("You forgot to enter your item");
-        }
-        //check to make sure guess is not NaN
-        else if(isNaN(guess)) {
-            alert("You must enter a number");
-        }
-        //check to make sure guess is between 1 and 100
-        else if(guess <= 0 && guess > 100) {
-            alert("You must enter a number between 1 and 100");
-        }
-        //If all well, compare guess to random
-        else { */
-            compareGuess(+guess); //not sure this goes here
-            console.log("Your guess is " + guess);
-            //}
+    /*--- Accept Guess ---*/
+    if (finish === false) {
+        $("#guessButton").click(getGuess);
+    }
+    else {
+        $('#feedback').text("You have already won, silly! Please click\"New Game\"");
     }
 
-    //Accept guess
-    $("#guessButton").click(validateGuess);
-        console.log("The guess is " + guess);
-    /*make enter submit button - this was happening automatically, so I disabled
     $("#userGuess").keyup(function(event) {
         if(event.keyCode == 13){
             $("#guessButton").click();
         }
-    });*/
-
+    });
+   
+  /*--- Validate and Process Guess---*/
     
+    function getGuess () {
+        guess = +$("#userGuess").val();
+        console.log("Guess is " + guess);
+        if(!guess || isNaN(guess) || guess <1 || guess >100) {
+         $('#feedback').text("You must enter a number between 1 and 100");
+         return false;
+        }
+        else compareGuess(guess);
+    }
     
     function compareGuess(x) {
-        diff = x - target;
+        diff = Math.abs(x - target);
             if (diff === 0) {
-                console.log("Yay!!!!!");
+                $('#feedback').text("You guessed correctly! Yay!");
+                addGuess();
+                finish = true;
+                endGame(finish);
             }
             else if (diff < 10) {
-                console.log("Diff less than 10");
+                $('#feedback').text("You are getting hotter!");
+                addGuess();
             }
             else if (diff < 25) {
-                console.log("Diff less than 25");
+                $('#feedback').text("You are sort of warm");
+                addGuess();
             }
             else if (diff < 40) {
-                console.log("Diff less than 40");
+                $('#feedback').text("You are barely warm");
+                addGuess();
             }
             else {
-                console.log("Diff greater than 40");
+                $('#feedback').text("You are pretty cold");
+                addGuess();
             }
-            numGuesses++;
-            console.log("The number of total guesses is " + numGuesses);
-            $('#guessList').append("<li>" + x + "</li>");
-            
-        }
-   });
+       }
+
+     function addGuess() {
+       numGuesses++;
+        console.log("The number of total guesses is " + numGuesses);
+        $('#guessList').append("<li>" + guess + "</li>");
+        $('#count').text(numGuesses);
+        $("#userGuess").val('');
+        $("#userGuess").focus();
+     }
+
+    
+    function endGame(finish) {
+         $('#guessButton').prop('disabled', finish);
+         $('#userGuess').prop('disabled', finish);
+    }
+
+    function newGame() {
+        numGuesses = 0;
+        endGame(false);
+        finish = false;
+        $('#guessList').children("li").remove();
+        target = null;
+        guess = null;
+        diff = null;
+        $('#feedback').text("Welcome!Make your Guess");
+        $('#count').text(numGuesses);
+        $("#userGuess").val('');
+        $("#userGuess").focus();
+    }
+});
 
   
-    
-        
-
-    
-        //$("#newitem").val(''); //clear new item entry box
    
-        //Check to make sure guess is between 1 and 100
-        //Display message if not
-    //Assign variable to guess
-    //Compare guess to random number
-    //Display appropriate response to guess
-    //Store guess value and display -
-    //Add to guess count
-    //repeat accept guess step
-    //when guess = random
-        //display win message
-        //display score
-        //end game
-    //If new game button clicked start at top
-	/*--- Display information modal box ---
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
-
-  	});
-
-  	/*--- Hide information modal box ---
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});*/
+    
 
 
 
